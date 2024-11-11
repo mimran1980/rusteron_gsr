1640764a3990ce520ff086a6074eef74bd1cd632
Mo Imran <mimran1980@gmail.com>
2024-11-11 10:14:00 +0000

    working example of rusteron-achive

--- rusteron-archive/src/lib.rs
+++ rusteron-archive/src/lib.rs
@@ -296,7 +296,7 @@ mod tests {
             control_channel: &str,
         ) -> io::Result<Self> {
             let gradle = if cfg!(target_os = "windows") {
-                "./gradlew.bat"
+                "gradlew.bat"
             } else {
                 "./gradlew"
             };
