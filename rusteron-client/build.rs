badc94a49e748c6c7ba9439fc057c06d71b179de
Mo Imran <mimran1980@gmail.com>
2025-01-10 12:37:08 +0000

    revert release change

--- rusteron-client/build.rs
+++ rusteron-client/build.rs
@@ -35,19 +35,9 @@ impl LinkType {
     }
 }
 
-// fn get_aeron_version(aeron_path: &Path) -> std::io::Result<String> {
-//     let version_file = aeron_path.join("version.txt");
-//     let mut file = fs::File::open(version_file)?;
-//     let mut version = String::new();
-//     file.read_to_string(&mut version)?;
-//     Ok(version.trim().to_string())
-// }
-
 pub fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=bindings.h");
-    println!("cargo:rerun-if-changed=aeron/version.txt");
-    println!("cargo:rerun-if-changed=.git");
 
     if pkg_config::probe_library("uuid").is_err() {
         eprintln!("uuid lib not found in path");
