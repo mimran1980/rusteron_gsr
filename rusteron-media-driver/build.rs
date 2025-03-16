785384f097232dfdc80a8d96e3d61f87efce271e
Mo Imran <mohammed.imran@gsr.io>
2025-03-16 20:55:26 +0000

    try reduce size

--- rusteron-media-driver/build.rs
+++ rusteron-media-driver/build.rs
@@ -273,7 +273,8 @@ pub fn main() {
     }
 
     #[cfg(feature = "static")]
-    if std::env::var("PUBLISH_ARTIFACTS").is_ok()  {
+    // media driver libs are too big there is 10meg limit so only do mac os
+    if std::env::var("PUBLISH_ARTIFACTS").is_ok() && !cfg!(target_os = "linux") {
         let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
         let cmake_lib_dir = cmake_output;
         publish_artifacts(&out_path, &cmake_lib_dir).expect("Failed to publish artifacts");
